1684705a1d47897a9e2a8d2c502eb413bfdfaa11
Joshua Hardman <jhardman0830@gmail.com>
2017-02-01 12:53:49 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintainance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public')); //takes absolute path
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  // res.send({
+  //   name: 'Josh',
+  //   like: [
+  //     'Breakdance',
+  //     'JS'
+  //   ]
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+      pageTitle: 'About Page'
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
